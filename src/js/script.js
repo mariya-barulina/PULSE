a9e5902d778322dc73d733094ab5543f12fa9785
Mariya Barulina <mariya.barulina.gracheva@gmail.com>
2022-04-13 21:29:33 +0300

    Adaptive slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
     $('.watch__inner').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/leftarrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/rightarrow.png"></button>'
-    });
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/rightarrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
+    });    
   });
+  
