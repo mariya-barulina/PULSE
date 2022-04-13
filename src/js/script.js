653e9f81595cc624390cb5d62d5e6b4afa76fc8b
Mariya Barulina <mariya.barulina.gracheva@gmail.com>
2022-04-13 17:28:45 +0300

    Add slider

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.watch__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/leftarrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/rightarrow.png"></button>'
+    });
+  });
